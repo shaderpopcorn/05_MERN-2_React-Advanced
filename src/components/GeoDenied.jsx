a0f892088c6caa3c32ebd10c447307ed33202fba
shaderpopcorn <kwmail@gmx.de>
2023-10-11 18:05:27 +0200

    loading, denied, css

--- src/components/GeoDenied.jsx
+++ src/components/GeoDenied.jsx
@@ -5,7 +5,13 @@ const GeoDenied = () => {
   return (
     <div className="card-geoDenied">
       <div className="data-geoDenied">
-        <p className="description-geoDenied">GeoDenied...</p>
+        <p className="description-geoDenied">Location access denied!</p>
+        <hr className="description-line" />
+        <p className="description-details">
+          For a good user experience on this website, <br />
+          please reload the page and allow the browser <br />
+          to use your location.
+        </p>
       </div>
       <div className="image-geoDenied">
         <SwitchImage iconUrl={"geoDenied"} />
